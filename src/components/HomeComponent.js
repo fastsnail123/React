66a7e3a56a1450da72949a60a939e6d92423d1a4
Yichen Li <957143430lyc@gmail.com>
2020-10-16 12:44:23 +1100

    Fetch from server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -8,6 +8,7 @@ import {
   CardTitle,
   CardSubtitle,
 } from "reactstrap";
+import {baseUrl} from '../shared/baseUrl';
 
 function RenderCard({ item,isLoading, errMess }) {
   if(isLoading){
@@ -22,7 +23,7 @@ function RenderCard({ item,isLoading, errMess }) {
   }
   else return (
     <Card>
-      <CardImg src={item.image} alt={item.name} />
+      <CardImg src={baseUrl + item.image} alt={item.name} />
       <CardBody>
         <CardTitle>{item.name}</CardTitle>
         {item.designation ? (<CardSubtitle>{item.designation}</CardSubtitle>) : null}
@@ -41,7 +42,9 @@ function Home(props) {
           errMess={props.dishesErrMess}/>
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.promotion} />
+          <RenderCard item={props.promotion}
+          isLoading={props.promosLoading} 
+          errMess={props.promosErrMess} />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.leader} />
